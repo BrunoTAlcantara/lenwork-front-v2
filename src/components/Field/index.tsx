2e5d19283d4a9590d148b8f66fe20d76c4bd81ce
Bruno Theodoro <brunotheodoro123@gmail.com>
2024-02-13 00:33:36 -0300

    feat/component field

---
+++ src/components/Field/index.tsx
@@ -0,0 +1,23 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { Input } from "../Input";
+
+interface InputProps {
+  label: string;
+  name: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+}
+
+const Field: React.FC<InputProps> = ({ label, name, register, errors }) => {
+  return (
+    <Input.Root>
+      <Input.Label label={label} />
+      <Input.Input {...register(name)} error={!!errors[name]} />
+      {errors[name] && (
+        <Input.Error error={errors?.[name]?.message?.toString()} />
+      )}
+    </Input.Root>
+  );
+};
+
+export default Field;
